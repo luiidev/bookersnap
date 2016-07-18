7bbb0f2889133edd9b9364bc7cfbea827da1648a
marxquva <marxquva@gmail.com>
2016-07-18 18:14:16 -0500

    ViewTurno

--- public/js/app/turno/turno.config.js
+++ public/js/app/turno/turno.config.js
@@ -3,15 +3,15 @@ angular.module('turno.app', ['turno.controller','turno.service','turno.directive
 	$stateProvider
 		.state ('turno', {
 			url: '/turno',
-			templateUrl: 'js/app/turno/view/index.html'
+			templateUrl: '/js/app/turno/view/index.html'
 		})
 		.state ('turno.activo', {
                 url: '/turno-activo',
-                templateUrl: 'js/app/turno/view/turno-activo.html'
+                templateUrl: '/js/app/turno/view/turno-activo.html'
         })
         .state ('turno.inactivo', {
                 url: '/turno-inactivo',
-                templateUrl: 'js/app/turno/view/turno-inactivo.html'
+                templateUrl: '/js/app/turno/view/turno-inactivo.html'
         })
 
 });
