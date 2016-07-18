24f57b31b2dbba279f64ae16f694ada576425e4e
marxquva <marxquva@gmail.com>
2016-07-18 18:07:20 -0500

    mergeestructura

--- public/js/app/turno/turno.config.js
+++ public/js/app/turno/turno.config.js
@@ -5,5 +5,13 @@ angular.module('turno.app', ['turno.controller','turno.service','turno.directive
 			url: '/turno',
 			templateUrl: 'js/app/turno/view/index.html'
 		})
+		.state ('turno.activo', {
+                url: '/turno-activo',
+                templateUrl: 'js/app/turno/view/turno-activo.html'
+        })
+        .state ('turno.inactivo', {
+                url: '/turno-inactivo',
+                templateUrl: 'js/app/turno/view/turno-inactivo.html'
+        })
 
 });
