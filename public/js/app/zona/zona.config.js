a5212865f53ec75fd034712d8af282c7710eae15
joper30 <joper30@gmail.com>
2016-07-18 18:42:35 -0500

    viewZonas

--- public/js/app/zona/zona.config.js
+++ public/js/app/zona/zona.config.js
@@ -5,5 +5,12 @@ angular.module('zona.app', ['zona.controller','zona.service','zona.directive'])
 			url: '/zona',
 			templateUrl: '/js/app/zona/view/index.html'
 		})
-
+		.state ('zona.activo', {
+			url: '/zona-activo',
+			templateUrl: '/js/app/zona/view/zona-activo.html'
+		})
+		.state ('zona.inactivo', {
+			url: '/zona-inactivo',
+			templateUrl: '/js/app/zona/view/zona-inactivo.html'
+		})
 });
