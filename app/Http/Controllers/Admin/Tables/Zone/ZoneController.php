20566c18289aa97fcbfb4c2f43389d0940368203
joper30 <joper30@gmail.com>
2016-07-19 18:34:07 -0500

    viewsZoneAndViewCreate

--- app/Http/Controllers/Admin/Tables/Zone/ZoneController.php
+++ app/Http/Controllers/Admin/Tables/Zone/ZoneController.php
@@ -6,43 +6,176 @@ use App\Http\Controllers\Controller as Controller;
 class ZoneController extends Controller
 {
    public function index(){
-      return response()->json(
-         array(
-           'zones' => array(
-             array(
-               'zone_id' => 1,
-               'zone_name' => 'vip',
-               'zone_price' => 0,
-               'zone_status' => true,
-               'tables' => array(
-                 array(
-                   'table_id' => 1,
-                   'table_name' => 'mesa 1',
-                   'table_size' => 3,
-                   'table_rotate' => 1),
-                 array(
-                   'table_id' => 2,
-                   'table_name' => 'mesa 2',
-                   'table_size' => 2,
-                   'table_rotate' => 2))),
-             array(
-               'zone_id' => 2,
-               'zone_name' => 'supervip',
-               'zone_price' => 0,
-               'zone_status' => true,
-               'tables' => array(
-                 array(
-                   'table_id' => 1,
-                   'table_name' => 'mesa 1',
-                   'table_size' => 3,
-                   'table_rotate' => 1),
-                 array(
-                   'table_id' => 2,
-                   'table_name' => 'mesa 2',
-                   'table_size' => 2,
-                   'table_rotate' => 2)
-                 ))
-             )));
+    return response()->json(
+     array (
+  0 => 
+  array (
+    'id' => 1,
+    'created_at' => '2016-07-19 16:42:45',
+    'updated_at' => '2016-07-19 16:42:45',
+    'name' => 'zona10',
+    'sketch' => 'asdas322fsd',
+    'status' => 1,
+    'type_zone' => 1,
+    'join_table' => 1,
+    'status_smoker' => 0,
+    'people_standing' => 0,
+    'user_add' => 1,
+    'user_upd' => 2,
+    'ev_event_id' => 11,
+    'ms_microsite_id' => 1,
+    'tables' => 
+    array (
+      0 => 
+      array (
+        'id' => 1,
+        'created_at' => '2016-07-19 17:26:02',
+        'updated_at' => '2016-07-19 17:26:02',
+        'res_zone_id' => 1,
+        'name' => '',
+        'min_cover' => 1,
+        'max_cover' => 0,
+        'price' => 0,
+        'status' => 1,
+        'config_color' => '',
+        'config_position' => '',
+        'config_forme' => 0,
+        'config_size' => 0,
+        'config_rotation' => 0,
+        'date_add' => '0000-00-00 00:00:00',
+        'date_upd' => '0000-00-00 00:00:00',
+        'user_add' => 0,
+        'user_upd' => 0,
+      ),
+      2 => 
+      array (
+        'id' => 3,
+        'created_at' => '2016-07-19 17:26:16',
+        'updated_at' => '2016-07-19 17:26:16',
+        'res_zone_id' => 1,
+        'name' => '',
+        'min_cover' => 1,
+        'max_cover' => 0,
+        'price' => 0,
+        'status' => 1,
+        'config_color' => '',
+        'config_position' => '',
+        'config_forme' => 0,
+        'config_size' => 0,
+        'config_rotation' => 0,
+        'date_add' => '0000-00-00 00:00:00',
+        'date_upd' => '0000-00-00 00:00:00',
+        'user_add' => 0,
+        'user_upd' => 0,
+      ),
+      3 => 
+      array (
+        'id' => 4,
+        'created_at' => '2016-07-19 18:21:21',
+        'updated_at' => '2016-07-19 18:21:21',
+        'res_zone_id' => 1,
+        'name' => 'table 4',
+        'min_cover' => 5,
+        'max_cover' => 0,
+        'price' => 0,
+        'status' => 1,
+        'config_color' => '1',
+        'config_position' => '1',
+        'config_forme' => 1,
+        'config_size' => 1,
+        'config_rotation' => 45,
+        'date_add' => '0000-00-00 00:00:00',
+        'date_upd' => '0000-00-00 00:00:00',
+        'user_add' => 1,
+        'user_upd' => 1,
+      ),
+    ),
+  ),
+  1 => 
+  array (
+    'id' => 2,
+    'created_at' => '2016-07-19 16:42:45',
+    'updated_at' => '2016-07-19 16:42:45',
+    'name' => 'zona11',
+    'sketch' => 'asdas322fsd',
+    'status' => 2,
+    'type_zone' => 1,
+    'join_table' => 1,
+    'status_smoker' => 0,
+    'people_standing' => 0,
+    'user_add' => 1,
+    'user_upd' => 2,
+    'ev_event_id' => 11,
+    'ms_microsite_id' => 1,
+    'tables' => 
+    array (
+      0 => 
+      array (
+        'id' => 1,
+        'created_at' => '2016-07-19 17:26:02',
+        'updated_at' => '2016-07-19 17:26:02',
+        'res_zone_id' => 1,
+        'name' => '',
+        'min_cover' => 1,
+        'max_cover' => 0,
+        'price' => 0,
+        'status' => 1,
+        'config_color' => '',
+        'config_position' => '',
+        'config_forme' => 0,
+        'config_size' => 0,
+        'config_rotation' => 0,
+        'date_add' => '0000-00-00 00:00:00',
+        'date_upd' => '0000-00-00 00:00:00',
+        'user_add' => 0,
+        'user_upd' => 0,
+      ),
+      2 => 
+      array (
+        'id' => 3,
+        'created_at' => '2016-07-19 17:26:16',
+        'updated_at' => '2016-07-19 17:26:16',
+        'res_zone_id' => 1,
+        'name' => '',
+        'min_cover' => 1,
+        'max_cover' => 0,
+        'price' => 0,
+        'status' => 1,
+        'config_color' => '',
+        'config_position' => '',
+        'config_forme' => 0,
+        'config_size' => 0,
+        'config_rotation' => 0,
+        'date_add' => '0000-00-00 00:00:00',
+        'date_upd' => '0000-00-00 00:00:00',
+        'user_add' => 0,
+        'user_upd' => 0,
+      ),
+      3 => 
+      array (
+        'id' => 4,
+        'created_at' => '2016-07-19 18:21:21',
+        'updated_at' => '2016-07-19 18:21:21',
+        'res_zone_id' => 1,
+        'name' => 'table 4',
+        'min_cover' => 5,
+        'max_cover' => 0,
+        'price' => 0,
+        'status' => 1,
+        'config_color' => '1',
+        'config_position' => '1',
+        'config_forme' => 1,
+        'config_size' => 1,
+        'config_rotation' => 45,
+        'date_add' => '0000-00-00 00:00:00',
+        'date_upd' => '0000-00-00 00:00:00',
+        'user_add' => 1,
+        'user_upd' => 1,
+      ),
+    ),
+  ),
+)
+      );
    }
 
    public function getZone($lang,$id){
--- public/js/app/tables/zone/zone.controller.js
+++ public/js/app/tables/zone/zone.controller.js
@@ -2,12 +2,35 @@ angular.module('zone.controller', [])
 
 .controller('ZoneCtrl', function($scope,ZoneFactory) {
 
-	$scope.zones = {};
+	$scope.zonesActive = {};
 
 	$scope.getZones = function(){
 
 		ZoneFactory.getZones().success(function(data){
-			console.log(JSON.stringify(data));
+			
+			var vZonesActive = [];
+			var vZonesInactive = [];
+			
+			angular.forEach(data, function(zones) {
+				console.log("datas" +JSON.stringify(zones.status));
+
+				var vTables = 0;
+
+				angular.forEach(zones.tables, function(tables) {
+					vTables + = 1;
+				});
+
+				zones.tables_count = vTables;
+				if (zones.status == "0" || zones.status == "2") {
+					vZonesInactive.push(zones);
+				}else{
+					vZonesActive.push(zones);
+				}
+				
+			});
+
+			$scope.zonesActive = vZonesActive;
+			$scope.zonesInactive = vZonesInactive;
 		});
 	};
 
