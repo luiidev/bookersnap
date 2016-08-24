c629cb38846a7cfe283b9a1632ffe86d7f923452
obedtrejolopez <obedtrejolopez@gmail.com>
2016-08-23 19:05:25 -0500

    URL por defecto

--- public/js/app/reservation/reservation.config.js
+++ public/js/app/reservation/reservation.config.js
@@ -8,5 +8,5 @@ angular.module('reservation.app',
 .constant("IdMicroSitio",idMicrositio)
 //.constant("ApiUrlReservation", 'http://web.aplication.bookersnap/v1/en/admin/ms/'+idMicrositio+'/mesas');
 .constant("ApiUrlReservation", "http://web.aplication.bookersnap/v1/en/admin/ms/"+idMicrositio+"/reservation")
-.constant("ApiUrlGeneral", "http://192.168.0.107/api.promociones/public/v1/es/")
-.constant("ApiUrlGeneralPromociones", "http://192.168.0.107/api.promociones/public/v1/es/microsites/"+idMicrositio);
+.constant("ApiUrlGeneral", "http://api-promotion.vh/v1/es/")
+.constant("ApiUrlGeneralPromociones", "http://api-promotion.vh/v1/es/microsites/"+idMicrositio);
