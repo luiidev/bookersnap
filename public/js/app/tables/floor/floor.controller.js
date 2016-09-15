b9a7a3e129119978cfe35b0563208e6b0168d125
obedtrejolopez <obedtrejolopez@gmail.com>
2016-09-15 16:02:44 -0500

    Crud de servers

--- public/js/app/tables/floor/floor.controller.js
+++ public/js/app/tables/floor/floor.controller.js
@@ -94,12 +94,114 @@ angular.module('floor.controller', [])
 
 })
 
-.controller('serverController', function($scope, ServerFactory) {
+.controller('serverController', function($scope, ServerFactory, ColorFactory) {
+
 	var sm = this;
 	sm.servers = [];
+	sm.tables = [{id:1},{id:2},{id:3},{id:4},{id:5}]; // El array ingresa de la lista de pruebas
+	sm.flagServer = false; 
+	sm.data = [];
+	/*
 	ServerFactory.getAllServer().then(function(response){
 		sm.servers = response.data.data;
-		console.log(sm.servers);
 	});
 
+	sm.colors= ColorFactory.getColor();
+
+	sm.selectColor =  function(color){
+
+		sm.color = color.colorHexadecimal;
+		var position = sm.colors.indexOf(color);
+		for(var i=0; i < sm.colors.length; i++) {
+			sm.colors[i].classSelect = "";	
+		}
+		sm.colors[position].classSelect = "is-selected";
+
+	};
+
+	sm.editServer =  function(server){
+
+		sm.flagServer = true;
+		var position=sm.servers.indexOf(server);
+		sm.server = sm.servers[position];
+		sm.server.name = sm.server.name;
+
+		for(var i=0; i < sm.colors.length; i++){
+			sm.colors[i].classSelect = "";
+			if(sm.colors[i].colorHexadecimal == sm.servers[position].color){
+				sm.color = sm.servers[position].color;
+				sm.colors[i].classSelect = "is-selected";
+			}
+		}
+
+	};
+
+	var limpiarData = function(){
+
+		sm.server.name = "";
+		for(var i=0; i < sm.colors.length; i++){
+			sm.colors[i].classSelect = "";
+		}
+
+	};
+
+	sm.saveOrUpdateServer = function(){
+
+		if(sm.flagServer == false){
+
+			sm.data = {
+				name : sm.server.name,
+				color : sm.color,
+				tables: sm.tables
+			};
+			
+			ServerFactory.addServer(sm.data).then(function(response){
+
+				if(response.data.response == false){
+					var mensaje = setearJsonError(response.data.jsonError);
+					messageAlert("Warning", mensaje,"warning", 3000);
+				}else if(response.data.success == true) {
+					var mensaje = response.data.msg;
+					messageAlert("success", mensaje,"success", 3000);
+					sm.servers.push(response.data.data);
+					limpiarData();
+				}
+
+			});
+
+		}else if(sm.flagServer == true){
+			
+			console.log(sm.server);
+			
+			sm.data = {
+				id : sm.server.id,
+				name : sm.server.name,
+				color : sm.color,
+				tables: sm.tables
+			};
+			
+			ServerFactory.updateServer(sm.data, sm.server.id).then(function(response){
+
+				if(response.data.response == false){
+					var mensaje = setearJsonError(response.data.jsonError);
+					messageAlert("Warning", mensaje,"warning", 3000);
+				}else if(response.data.success == true) {
+					var mensaje = response.data.msg;
+					messageAlert("success", mensaje,"success", 3000);
+					sm.servers.push(response.data.data);
+					limpiarData();
+				}
+
+			});
+			
+		}
+	}
+
+	sm.cancelEditServer =  function(server){
+
+		sm.flagServer = false;
+		limpiarData();
+
+	};
+	*/
 });
--- public/js/app/tables/floor/server.service.js
+++ public/js/app/tables/floor/server.service.js
@@ -4,5 +4,57 @@ angular.module('server.service', [])
 			getAllServer: function(){
 				return $http.get(ApiUrlMesas+"/servers"); 
 			},
+			addServer: function(data){
+				return $http({url:ApiUrlMesas+"/servers", method: "POST", data: data}).then(function successCallback(response) {
+						    return response;
+						  }, function errorCallback(response) {
+						  	return response;
+						  })
+			},
+			updateServer: function(data, id_server){
+				return $http({url:ApiUrlMesas+"/servers/"+id_server, method: "PUT", data: data}).then(function successCallback(response) {
+						    return response;
+						  }, function errorCallback(response) {
+						  	return response;
+						  })
+			},
 		}
 	})
+	.factory('ColorFactory', function() {
+		return {
+			getColor: function(){
+				 return colors = [
+				 	{ colorHexadecimal: "#9F7421"},
+				 	{ colorHexadecimal: "#82d1d3"},
+				 	{ colorHexadecimal: "#7c004c"},
+				 	{ colorHexadecimal: "#e176ce"},
+				 	{ colorHexadecimal: "#7e3c91"},
+				 	{ colorHexadecimal: "#b30011"},
+				 	{ colorHexadecimal: "#1e9084"},
+				 	{ colorHexadecimal: "#ffffe8"},
+				 	{ colorHexadecimal: "#eead00"},
+				 	{ colorHexadecimal: "#ff6138"},
+				 	{ colorHexadecimal: "#007fff"},
+				 	{ colorHexadecimal: "#ffc4b5"},
+				 	{ colorHexadecimal: "#a9d4ff"},
+				 	{ colorHexadecimal: "#30c6bd"},
+				 	{ colorHexadecimal: "#5fefe7"},
+				 	{ colorHexadecimal: "#ffc26b"},
+				 	{ colorHexadecimal: "#00aeef"},
+				 	{ colorHexadecimal: "#cdc3ff"},
+				 	{ colorHexadecimal: "#9be4ff"},
+				 	{ colorHexadecimal: "#aa0d71"},
+				 	{ colorHexadecimal: "#e891d7"},
+				 	{ colorHexadecimal: "#fc00ff"},
+				 	{ colorHexadecimal: "#901eac"},
+				 	{ colorHexadecimal: "#c41200"},
+				 	{ colorHexadecimal: "#ffffff"},
+				 	{ colorHexadecimal: "#2c3e50"},
+				 	{ colorHexadecimal: "#ab7714"},
+				 	{ colorHexadecimal: "#755e33"},
+				 	{ colorHexadecimal: "#83ff5a"},
+				 	{ colorHexadecimal: "#2085d8"},
+				 	];
+			},
+		}
+	});
