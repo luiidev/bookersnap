e28e9fc0e551d2eb99c1ab590d4458a6ea1f9866
joper30 <joper30@gmail.com>
2016-09-22 12:15:08 -0500

    update blocks

--- public/js/app/tables/block/block.config.js
+++ public/js/app/tables/block/block.config.js
@@ -1,13 +1,12 @@
-angular.module('block.app', ['block.controller','block.service','block.directive'])
-.config(function ($stateProvider, $urlRouterProvider){
-	$stateProvider
-		.state ('block', {
-			url: '/block/:date',
-			templateUrl: '/js/app/tables/block/view/create.html'
-		})
-		.state ('blockEdit', {
-			url: '/block/:date/:block_id',
-			templateUrl: '/js/app/tables/block/view/edit.html'
-		})
-    
-});
+angular.module('block.app', ['block.controller', 'block.service', 'block.directive'])
+	.config(function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('block', {
+				url: '/block/:date',
+				templateUrl: '/js/app/tables/block/view/create.html'
+			})
+			.state('blockEdit', {
+				url: '/block/:date/:block_id',
+				templateUrl: '/js/app/tables/block/view/edit.html'
+			})
+	});
