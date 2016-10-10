693103ae72f3985a8c73dc3967e78d40f0c33df9
joper30 <joper30@gmail.com>
2016-10-10 10:30:00 -0500

    zones

--- public/js/app/tables/block/block.config.js
+++ public/js/app/tables/block/block.config.js
@@ -1,17 +1,17 @@
-angular.module('block.app', ['block.controller','block.service','block.directive'])
-.config(function ($stateProvider, $urlRouterProvider){
-	$stateProvider
-		.state ('mesas.block', {
-			url: '/block/:date',
-			templateUrl: '/js/app/tables/block/view/create.old.html',
-                        controller: "blockCtr",
-                        cache: false
-		})
-		.state ('mesas.blockEdit', {
-			url: '/block/:date/:block_id',
-			templateUrl: '/js/app/tables/block/view/edit.new.html',
-                        controller: "blockCtrEdit",                        
-                        cache: false
-		})
-    
-});
+angular.module('block.app', ['block.controller', 'block.service', 'block.directive'])
+	.config(function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('mesas.block', {
+				url: '/block/:date',
+				templateUrl: '/js/app/tables/block/view/create.old.html',
+				controller: "blockCtr",
+				cache: false
+			})
+			.state('mesas.blockEdit', {
+				url: '/block/:date/:block_id',
+				templateUrl: '/js/app/tables/block/view/edit.new.html',
+				controller: "blockCtrEdit",
+				cache: false
+			});
+
+	});
