8417a9cc209e87fbf76ab184f4063ed472a1cab8
Cesar Moreno <stonblizzard@gmail.com>
2016-10-11 15:03:21 -0500

    Se corrigio servicio de tag custom guest y se implemento con vista

--- public/js/app/tables/customtag/customtag.controller.js
+++ public/js/app/tables/customtag/customtag.controller.js
@@ -1,15 +1,46 @@
 angular.module('customtag.controller', [])
 	.controller('CustomTagCtrl', function(CustomTagService) {
 		var vm = this;
+		vm.name = "";
 
 		vm.guestTagAll = function() {
-			console.log("test");
 			CustomTagService.getAllTag().then(function success(response) {
 				vm.guestTagList = response;
-				console.log(vm.guestTagList);
 			}, function error(response) {
-				// messageErrorApi(response.data, "Error", "warning", 0, true, response.status);
-				console.log("fail");
+				messageErrorApi(response, "Error", "warning");
+			});
+		};
+
+		vm.guestTagAll();
+
+		vm.guestCreateTag = function(name) {
+			vm.loading = true;
+			CustomTagService.createTag(name).then(function success(response) {
+				vm.loading = false;
+				data = response;
+				if (data != null) {
+					vm.name = "";
+					messageAlert("Success", "Se registro el Tag Correstamente", "success");
+					vm.guestTagList.push(data);
+				} else {
+					messageErrorApi(response, "Este Tag ya se encuentra regitrado", "warning");
+				}
+
+			}, function error(response) {
+				messageErrorApi(response, "Error", "warning");
+			});
+		};
+
+		vm.guestDeleteTag = function(id) {
+			vm.loading = true;
+			console.log(id);
+			CustomTagService.deleteTag(id).then(function success(response) {
+				vm.loading = false;
+				var index = CustomTagService.findWithAttr(vm.guestTagList, "id", id);
+				vm.guestTagList.splice(index, 1);
+			}, function(response) {
+				vm.loading = false;
+				messageErrorApi(response, "Error", "warning");
 			});
 		};
 
--- public/js/app/tables/customtag/customtag.service.js
+++ public/js/app/tables/customtag/customtag.service.js
@@ -2,11 +2,14 @@ angular.module('customtag.service', [])
 	.service('CustomTagDataService', function($http, ApiUrlMesas) {
 		return {
 			getListTagCustom: function() {
-
 				return $http.get(ApiUrlMesas + "/guest-tags");
 			},
 			createTagCustom: function(name) {
-				return $http.post(ApiUrlMesas + "/guest-tags/");
+				return $http.put(ApiUrlMesas + "/guest-tags", {}, {
+					params: {
+						name: name
+					}
+				});
 			},
 			deleteTagCustom: function(idTag) {
 				return $http.delete(ApiUrlMesas + "/guest-tags/" + idTag);
@@ -20,16 +23,44 @@ angular.module('customtag.service', [])
 				CustomTagDataService.getListTagCustom().success(function(data) {
 					data = data.data;
 					defered.resolve(data);
-				}).error(function(response) {
+				}).error(function(data, status, headers) {
+					var response = jsonErrorData(data, status, headers);
 					defered.reject(response);
 				});
 				return defered.promise;
 			},
-			createTag: function() {
+			createTag: function(name) {
+				var defered = $q.defer();
+				CustomTagDataService.createTagCustom(name).success(function(data) {
+					data = data.data;
+					defered.resolve(data);
+				}).error(function(data, status, headers) {
+					var response = jsonErrorData(data, status, headers);
+					defered.reject(response);
+				});
 
+				return defered.promise;
 			},
-			deleteTag: function() {
+			deleteTag: function(idTag) {
+				var defered = $q.defer();
+				var promise = defered.promise;
+				CustomTagDataService.deleteTagCustom(idTag).success(function(data) {
+					data = data.data;
+					defered.resolve(data);
+				}).error(function(data, status, headers) {
+					var response = jsonErrorData(data, status, headers);
+					defered.reject(response);
+				});
+				return promise;
+			},
 
+			findWithAttr: function(array, attr, value) {
+				for (var i = 0; i < array.length; i += 1) {
+					if (array[i][attr] === value) {
+						return i;
+					}
+				}
+				return -1;
 			},
 
 		};
