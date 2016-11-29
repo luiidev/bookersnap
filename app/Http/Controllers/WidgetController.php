ed41623beea5ba080cd8b82a0bd7d8c733f481cd
Luis Vasquez <luis.vasquez.dev@bookersnap.com>
2016-11-29 17:17:27 -0500

    avance de controladores y metodos de widget

---
+++ app/Http/Controllers/WidgetController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Requests\WidgetConfirmRequest;
+use App\Services\Helpers\ApiRequestsHelper;
+use Illuminate\Http\Request;
+use Validator;
+
+class WidgetController extends Controller
+{
+    public function index()
+    {
+        return view("widget.paso_1");
+    }
+
+    public function confirm(Request $request, $site, $date, $hour, $num)
+    {
+        $rules = [
+            "date"  => "required|date",
+            "hour" => "required|numeric|digits:4|hour",
+            "num"   => "required|integer|between:1,20"
+        ];
+
+        $validate = [
+            "date"  => $date,
+            "hour" => $hour,
+            "num"   => $num
+        ];
+
+        $validator =  Validator::make($validate, $rules);
+
+        if ($validator->fails()) {
+            return response("Pagina no encontrada", 404);
+        }
+
+        $params = [
+            "date" => "2016-11-29",
+            "hour" => "02:00:00",
+            "num_guests" => "20",
+            "next_day" => "1",
+            "zone_id" => "1",
+            "token" => "123456789"
+        ];
+
+        $url = "localhost:3004/v1/es/microsites/1/reservationtemporal?date=2016-11-29&hour=02:00:00&num_guests=20&next_day=1&zone_id=1&token=123456789";
+
+        $response = ApiRequestsHelper::SendRequest("GET", $url, []);
+
+        var_dump($response);
+
+        return $response;
+
+        return view("widget.paso_2");
+    }
+}
---
+++ resources/views/widget/paso_2.blade.php
@@ -0,0 +1,102 @@
+@extends('widget.layouts.main')
+
+@section("title", "Paramontino | Confirmar")
+
+@section("content")
+<div class="card">
+    <div class="card-body card-padding">
+
+        <div class="col-xs-12 col-sm-4 content-info">
+            <div class="reservation-info">
+                <div class="site-info">
+                    <h4 class="m-t-0 ">Paramontino</h4>
+                    <p>100 Queen St W, Toronto, ON M5H 2N2 (Downtown Core)</p>
+                </div>
+                <div class="info">
+                    <span>Tue, Nov 29 2016</span>
+                </div>
+                <div class="info">
+                    <span>11:30 AM</span>
+                </div>
+                <div class="info">
+                    <span>2 Guests</span>
+                </div>
+                <div class="info">
+                    <a href="javascript:void(0)">Change Reservation</a>
+                </div>
+                <div class="info watch">
+                    <span>Time to Complete</span>
+                    <div class="timer">
+                        <span>1:39</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-8 p-r-0">
+            <h4 class="m-t-0 c-blue">Confirmar Reservación</h4>
+            <form  name="availavilitySearchForm" novalidate>
+                <div class="row">
+
+                    <div class="col-sm-5">
+                        <label>Nombres</label>
+                        <div class="form-group">
+                            <div class="fg-line">
+                                <input type="text" class="form-control input-sm"  placeholder="Ingrese su nombre">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-7">
+                        <label>Apellidos</label>
+                        <div class="form-group">
+                            <div class="fg-line">
+                                <input type="text" class="form-control input-sm"  placeholder="Ingrese sus apellidos">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12">
+                        <label>Correo</label>
+                        <div class="form-group">
+                            <div class="fg-line">
+                                <input type="text" class="form-control input-sm"  placeholder="usuario@bookersnap.com">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12">
+                        <label>Telefono mobil</label>
+                        <div class="form-group">
+                            <div class="fg-line">
+                                <input type="text" class="form-control input-sm"  placeholder="55 555 555 555">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-xs-12">
+                        <label>Nota de reservación</label>
+                        <div class="form-group">
+                            <div class="fg-line">
+                                <input type="text" class="form-control input-sm"  placeholder="Alguna observacion">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 p-b-20">
+                        <p> Yes, I would like to receive marketing emails or promotions from this restaurant in the future.</p>
+                        <p>By confirming, you agree to Yelp Reservations' Terms Of Use & Privacy Policy.<br>
+                            Yelp stores this information and sends it to restaurants for transactional and promotional purposes. We’ll text you with reservation status updates – to stop receiving them, just text back "4" at any time. Note that we’ll text you with reservation status updates.</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12 m-b-20">
+                        <button class="btn btn-primary pull-right" >Confirmar reservación</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
+</div>
+@endsection
