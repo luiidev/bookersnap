c7dccca6a31fd0386ddf9b78ee1d650523de4bd3
Josue Diaz Rimache <josue.diaz.dev@bookersnap.com>
2016-12-14 18:59:25 -0500

    gridStylesLienzo

--- public/js/app/tables/grid/grid.controller.js
+++ public/js/app/tables/grid/grid.controller.js
@@ -1,4 +1,24 @@
 angular.module('grid.controller', [])
     .controller('GridCtrl', function($scope) {
+        var vm = this;
+
+        vm.hourIni = "";
+
+        var init = function() {
+            $(".selectable").selectable();
+        };
+
+        vm.initReservationCreate = function(hour_ini) {
+            console.log("initReservationCreate " + hour_ini);
+            vm.hourIni = hour_ini;
+        };
+
+        vm.selectTimeReservationCreate = function() {
+            if (vm.hourIni !== "") {
+                console.log("selectTimeReservationCreate");
+            }
+        };
+
+        init();
 
     });
