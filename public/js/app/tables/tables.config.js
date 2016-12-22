e998f6aaad8b4006163e28577308a3c36bebb32a
Cristofer Lazo <cjlr89@gmail.com>
2016-12-21 19:44:56 -0500

    update

--- public/js/app/tables/tables.config.js
+++ public/js/app/tables/tables.config.js
@@ -17,12 +17,12 @@
             'grid.app'
         ])
         .constant("IdMicroSitio", idMicrositio)
-        /*.constant("UrlServerNotify", "http://weblaravel.studework.com:1337")
+        .constant("UrlServerNotify", "http://weblaravel.studework.com:1337")
         .constant("ApiUrlMesas", 'http://weblaravel.studework.com/v1/es/microsites/' + idMicrositio)
-        .constant("ApiUrlRoot", 'http://weblaravel.studework.com/v1/es')*/
-        .constant("UrlServerNotify", "http://weblaravel.studework.vm:1337")
+        .constant("ApiUrlRoot", 'http://weblaravel.studework.com/v1/es')
+        /*.constant("UrlServerNotify", "http://weblaravel.studework.vm:1337")
         .constant("ApiUrlMesas", 'http://apimesas.studework.vm/v1/es/microsites/' + idMicrositio)
-        .constant("ApiUrlRoot", 'http://apimesas.studework.vm/v1/es')
+        .constant("ApiUrlRoot", 'http://apimesas.studework.vm/v1/es')*/
         .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
             $stateProvider
                 .state('mesas', {
