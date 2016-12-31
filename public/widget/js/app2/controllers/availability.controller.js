6f413aa8ecba958c4a88abc58db75036f9d4423d
Cristofer Lazo <cjlr89@gmail.com>
2016-12-30 22:44:56 -0500

    update

--- public/widget/js/app2/controllers/availability.controller.js
+++ public/widget/js/app2/controllers/availability.controller.js
@@ -315,7 +315,7 @@ angular.module("App")
             vm.promotion.imageUrl = item.image ? "url(" + item.image + ")" : null; 
             vm.promotion.description = item.description || null;
             vm.promotion.display = true;
-            angular.element(document.querySelector("#event")).css({top: evt.pageY - (vm.promotion.imageUrl ? 270 : 120) });
+            angular.element(document.querySelector("#event")).css({top: evt.pageY - (vm.promotion.imageUrl ? 190 : 120) });
         };
 
         vm.promotionHide = function() {
