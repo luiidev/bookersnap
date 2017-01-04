0878ffe4a2180669e2dc0173b40b7b2e9499969b
Luis Vasquez <luizhito.lp.4ever@gmail.com>
2017-01-04 16:54:15 -0500

    Se agrego compresion de archivos con elixir

--- public/js/app/tables/tables.config.js
+++ public/js/app/tables/tables.config.js
@@ -15,16 +15,17 @@
             'configuration.app',
             'availability.app',
             'grid.app',
-            'widget.app'
+            'widget.app',
+            'notification.app'
         ])
         .constant("IdMicroSitio", idMicrositio)
         /*.constant("UrlServerNotify", "http://weblaravel.studework.com:1337")
         .constant("ApiUrlMesas", 'http://apimesas.studework.com/v1/es/microsites/' + idMicrositio)
         .constant("ApiUrlRoot", 'http://apimesas.studework.com/v1/es')*/
-        .constant("DomainBookersnapAdmin", "http://weblaravel.studework.vm")
-        .constant("UrlServerNotify", "http://weblaravel.studework.vm:1337")
-        .constant("ApiUrlMesas", 'http://apimesas.studework.vm/v1/es/microsites/' + idMicrositio)
-        .constant("ApiUrlRoot", 'http://apimesas.studework.vm/v1/es')
+        .constant("DomainBookersnapAdmin", "http://localhost")
+        .constant("UrlServerNotify", "http://localhost:1337")
+        .constant("ApiUrlMesas", 'http://localhost:3004/v1/es/microsites/' + idMicrositio)
+        .constant("ApiUrlRoot", 'http://localhost:3004/v1/es')
         .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
             $stateProvider
                 .state('mesas', {
