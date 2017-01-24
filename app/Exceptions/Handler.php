43e357f1c22ad3f97aedadab6971feff40e2769f
Cristofer Lazo <cjlr89@gmail.com>
2017-01-24 11:31:02 -0500

    views error html and notification book modo reservation

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,8 +9,8 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
-class Handler extends ExceptionHandler
-{
+class Handler extends ExceptionHandler {
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -31,8 +31,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $e
      * @return void
      */
-    public function report(Exception $e)
-    {
+    public function report(Exception $e) {
         parent::report($e);
     }
 
@@ -43,8 +42,33 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $e
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $e)
-    {
-        return parent::render($request, $e);
+    public function render($request, Exception $e) {
+//        return parent::render($request, $e);
+        if ($this->isHttpException($e)) {
+            switch ($e->getStatusCode()) {
+
+                // not authorized
+                case 403:
+                    return \Response::view('errors.403', array(), 403);
+                    break;
+
+                // not found
+                case 404:
+                    return \Response::view('errors.404', array(), 404);
+                    break;
+
+                // internal error
+                case '500':
+                    return \Response::view('errors.500', array(), 500);
+                    break;
+
+                default:
+                    return $this->renderHttpException($e);
+                    break;
+            }
+        } else {
+            return parent::render($request, $e);
+        }
     }
+
 }
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error del servidor</title>
+</head>
+<body>
+Error interno del servidor.
+</body>
+</html>
